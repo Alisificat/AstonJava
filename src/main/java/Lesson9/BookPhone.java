06cbaeb6477c855f9e9597b3dae51efc75b97e12
Alicificat <roxanela@student.21-school.ru>
2024-07-04 19:23:47 +0300

    add all

--- src/main/java/Lesson9/BookPhone.java
+++ src/main/java/Lesson9/BookPhone.java
@@ -6,9 +6,27 @@ import java.util.*;
 public class BookPhone {
     private Map<String, List<String>> contactLog = new HashMap<>();
 
+//    public void add(String lastName, String phoneNumber) {
+//        if (contactLog.containsKey(lastName)) {
+//            contactLog.get(lastName).add(phoneNumber);
+//        } else {
+//            List<String> phoneNumbers = new ArrayList<>();
+//            phoneNumbers.add(phoneNumber);
+//            contactLog.put(lastName, phoneNumbers);
+//        }
+//    }
+//
+//    public List<String> get(String lastName) {
+//        return contactLog.getOrDefault(lastName, new ArrayList<>());
+//    }
+
+
     public void add(String lastName, String phoneNumber) {
         if (contactLog.containsKey(lastName)) {
-            contactLog.get(lastName).add(phoneNumber);
+            List<String> phoneNumbers = contactLog.get(lastName);
+            if (!phoneNumbers.contains(phoneNumber)) {
+                phoneNumbers.add(phoneNumber);
+            }
         } else {
             List<String> phoneNumbers = new ArrayList<>();
             phoneNumbers.add(phoneNumber);
@@ -17,6 +35,6 @@ public class BookPhone {
     }
 
     public List<String> get(String lastName) {
-        return contactLog.getOrDefault(lastName, new ArrayList<>());
+        return contactLog.getOrDefault(lastName, Collections.emptyList());
     }
 }
--- src/main/java/Lesson9/Main.java
+++ src/main/java/Lesson9/Main.java
@@ -1,5 +1,8 @@
 package Lesson9;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         // Задача 1
